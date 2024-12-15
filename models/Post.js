097e0129aadce6a7cc58c57d90fd1c68776f0e13
maazshaikh1711 <0maaz0shaikh0@gmail.com>
2024-12-15 23:05:16 +0530

    added "Post" model

---
+++ models/Post.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const PostSchema = new Schema(
+  {
+    user: { type: Schema.Types.ObjectId, ref: 'User', required: true },
+    caption: { type: String, required: true }, // Caption for the post
+    imageUrl: { type: String, required: false }, // Optional image URL
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model('Post', PostSchema);
