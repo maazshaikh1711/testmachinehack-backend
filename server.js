fd35fb187826b36881747e16aee4091c132a8db1
maazshaikh1711 <0maaz0shaikh0@gmail.com>
2024-12-15 21:14:47 +0530

    created server, connected mongodb

---
+++ server.js
@@ -0,0 +1,21 @@
+// server.js
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+
+// MongoDB connection
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('MongoDB connected'))
+  .catch(err => console.log(err));
+
+const server = app.listen(5000, () => {
+      console.log('Server running on port 5000');
+    });
