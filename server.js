c12826e16e231e45d3ec2db9bf7e27c7692a574f
maazshaikh1711 <0maaz0shaikh0@gmail.com>
2024-12-15 21:16:14 +0530

    added socket setup

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const socketio = require('socket.io');
 const dotenv = require('dotenv');
 
 dotenv.config();
@@ -9,7 +10,6 @@ dotenv.config();
 const app = express();
 app.use(express.json());
 app.use(cors());
-
 
 // MongoDB connection
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
@@ -19,3 +19,15 @@ mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopol
 const server = app.listen(5000, () => {
       console.log('Server running on port 5000');
     });
+
+
+// Socket.io setup
+const io = socketio(server);
+
+// Socket.io events
+io.on('connection', socket => {
+  console.log('New client connected');
+  socket.on('disconnect', () => console.log('Client disconnected'));
+});
+
+
